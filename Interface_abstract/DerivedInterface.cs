d3a5507d0eacf783861c1bbb0a16cb02d826f6a5
twozeronine <amdusias209@gmail.com>
2021-04-26 13:23:54 +0900

    MultiInterfaceInheritance.cs / 인터페이스 다중 상속

--- Interface_abstract/DerivedInterface.cs
+++ Interface_abstract/DerivedInterface.cs
@@ -21,12 +21,12 @@ class ConsoleLogger2 : IFormattableLogger
   }
 }
 
-class MainApp
-{
-  static void Main(string[] args)
-  {
-    IFormattableLogger logger = new ConsoleLogger2();
-    logger.WriteLog("The world is not flat");
-    logger.WriteLog("{0} + {1} = {2}", "1", 1, 2);
-  }
-}
+// class MainApp
+// {
+//   static void Main(string[] args)
+//   {
+//     IFormattableLogger logger = new ConsoleLogger2();
+//     logger.WriteLog("The world is not flat");
+//     logger.WriteLog("{0} + {1} = {2}", "1", 1, 2);
+//   }
+// }
