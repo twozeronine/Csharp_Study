32604cc19eae541d36678095ba57094901a1d6ef
twozeronine <amdusias209@gmail.com>
2021-04-26 13:30:32 +0900

    포함 기법을 사용해 클래스 다중 상속 흉내 내보기

--- Interface_abstract/MultiInterfaceInheritance.cs
+++ Interface_abstract/MultiInterfaceInheritance.cs
@@ -22,6 +22,41 @@ class FlyingCar : IRunnable, IFlyable
   }
 }
 
+#region Containment (포함) 기법을 사용하여 클래스 다중 상속 흉내 내보기
+
+class Plane : IFlyable
+{
+  public void Fly()
+  {
+    Console.WriteLine("Fly Fly");
+  }
+}
+class Car : IRunnable
+{
+  public void Run()
+  {
+    Console.WriteLine("Run Run");
+  }
+}
+
+class FlyingCar2 : IRunnable, IFlyable
+{
+  Car car = new Car();
+  Plane plane = new Plane();
+  public void Run()
+  {
+    car.Run();
+  }
+  public void Fly()
+  {
+    plane.Fly();
+  }
+}
+
+#endregion
+
+
+
 class MainApp
 {
   static void Main(string[] args)
@@ -35,5 +70,10 @@ class MainApp
 
     IFlyable flyable = car as IFlyable;
     flyable.Fly();
+
+    // 포함 기법을 사용한 클래스 다중 상속 흉내내보기
+    FlyingCar2 car2 = new FlyingCar2();
+    car2.Fly();
+    car2.Run();
   }
 }
